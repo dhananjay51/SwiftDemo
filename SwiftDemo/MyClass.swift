a411ffcfda2f2d16da654444960cc2b4343906f7
dhananjaysingh51 <dsloudshout.in>
2015-12-27 18:08:49 +0530

    add  classMethod

---
+++ SwiftDemo/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  SwiftDemo
+//
+//  Created by Abhishek Srivastava on 27/12/15.
+//  Copyright © 2015 LoudShout. All rights reserved.
+//
+
+import UIKit
+
+class MyClass: NSObject {
+    var  name:String!
+    func name(age:Int)
+    {
+      print( age)
+}
+
+}
